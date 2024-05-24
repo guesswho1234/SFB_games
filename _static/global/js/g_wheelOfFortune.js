da79a7ff73be956d642a5f8013c03af1fadbd287
guesswho1234 <sbachler@gmx.at>
2024-05-24 17:01:32 +0200

    fix: some minor style things

--- _static/global/js/g_wheelOfFortune.js
+++ _static/global/js/g_wheelOfFortune.js
@@ -8,6 +8,7 @@ $('.resultButton').click(function() {
 let segments = [];
 for (var i = 0; i < numSegments; i++) {
     segments.push({
+        // available fields: fillStyle, text, textFontSize, textFillStyle
         'fillStyle' : '#' + colors[i % colors.length],
         'text' : ''
     });
@@ -20,58 +21,8 @@ let theWheel = new Winwheel({
     'textFontSize'    : 24,                                                                                     // Set default font size for the segments.
     'textOrientation' : 'vertical',                                                                             // Make text vertial so goes down from the outside of wheel.
     'textAlignment'   : 'outer',                                                                                // Align text to outside of wheel.
-    'numSegments'     : numSegments,                                                                                     // Specify number of segments.
-    'segments'        : segments,                                                                              // Define segments including colour and text.
-//    [                                                                                                         // font size and test colour overridden on backrupt segments.
-//        {'fillStyle' : '#0077BB', 'text' : ''},
-//        {'fillStyle' : '#E8601C', 'text' : ''},
-//        {'fillStyle' : '#0077BB', 'text' : ''},
-//        {'fillStyle' : '#E8601C', 'text' : ''},
-//        {'fillStyle' : '#0077BB', 'text' : ''},
-//        {'fillStyle' : '#E8601C', 'text' : ''},
-//        {'fillStyle' : '#0077BB', 'text' : ''},
-//        {'fillStyle' : '#E8601C', 'text' : ''},
-//        {'fillStyle' : '#0077BB', 'text' : ''},
-//        {'fillStyle' : '#E8601C', 'text' : ''},
-//        {'fillStyle' : '#0077BB', 'text' : ''},
-//        {'fillStyle' : '#E8601C', 'text' : ''},
-//        {'fillStyle' : '#0077BB', 'text' : ''},
-//        {'fillStyle' : '#E8601C', 'text' : ''},
-//        {'fillStyle' : '#0077BB', 'text' : ''},
-//        {'fillStyle' : '#E8601C', 'text' : ''},
-//        {'fillStyle' : '#0077BB', 'text' : ''},
-//        {'fillStyle' : '#E8601C', 'text' : ''},
-//        {'fillStyle' : '#0077BB', 'text' : ''},
-//        {'fillStyle' : '#E8601C', 'text' : ''},
-//        {'fillStyle' : '#0077BB', 'text' : ''},
-//        {'fillStyle' : '#E8601C', 'text' : ''},
-//        {'fillStyle' : '#0077BB', 'text' : ''},
-//        {'fillStyle' : '#E8601C', 'text' : ''}
-//       {'fillStyle' : '#ee1c24', 'text' : '300'},
-//       {'fillStyle' : '#3cb878', 'text' : '450'},
-//       {'fillStyle' : '#f6989d', 'text' : '600'},
-//       {'fillStyle' : '#00aef0', 'text' : '750'},
-//       {'fillStyle' : '#f26522', 'text' : '500'},
-//       {'fillStyle' : '#000000', 'text' : 'BANKRUPT', 'textFontSize' : 16, 'textFillStyle' : '#ffffff'},
-//       {'fillStyle' : '#e70697', 'text' : '3000'},
-//       {'fillStyle' : '#fff200', 'text' : '600'},
-//       {'fillStyle' : '#f6989d', 'text' : '700'},
-//       {'fillStyle' : '#ee1c24', 'text' : '350'},
-//       {'fillStyle' : '#3cb878', 'text' : '500'},
-//       {'fillStyle' : '#f26522', 'text' : '800'},
-//       {'fillStyle' : '#a186be', 'text' : '300'},
-//       {'fillStyle' : '#fff200', 'text' : '400'},
-//       {'fillStyle' : '#00aef0', 'text' : '650'},
-//       {'fillStyle' : '#ee1c24', 'text' : '1000'},
-//       {'fillStyle' : '#f6989d', 'text' : '500'},
-//       {'fillStyle' : '#f26522', 'text' : '400'},
-//       {'fillStyle' : '#3cb878', 'text' : '900'},
-//       {'fillStyle' : '#000000', 'text' : 'BANKRUPT', 'textFontSize' : 16, 'textFillStyle' : '#ffffff'},
-//       {'fillStyle' : '#a186be', 'text' : '600'},
-//       {'fillStyle' : '#fff200', 'text' : '700'},
-//       {'fillStyle' : '#00aef0', 'text' : '800'},
-//       {'fillStyle' : '#ffffff', 'text' : 'LOOSE TURN', 'textFontSize' : 12}
-//    ],
+    'numSegments'     : numSegments,                                                                            // Specify number of segments.
+    'segments'        : segments,                                                                               // Define segments including colour and text.
     'animation' :                                                                                               // Specify the animation to use.
     {
         'type'     : 'spinToStop',
@@ -103,10 +54,10 @@ function startSpin()
 
 function resetWheel()
 {
-    theWheel.stopAnimation(false);  // Stop the animation, false as param so does not call callback function.
-//    theWheel.rotationAngle = 0;     // Re-set the wheel angle to 0 degrees.
-    theWheel.draw();                // Call draw to render changes to the wheel.
-    wheelSpinning = false;          // Reset to false to power buttons and spin can be clicked again.
+    theWheel.stopAnimation(false);      // Stop the animation, false as param so does not call callback function.
+//    theWheel.rotationAngle = 0;       // Re-set the wheel angle to 0 degrees.
+    theWheel.draw();                    // Call draw to render changes to the wheel.
+    wheelSpinning = false;              // Reset to false to power buttons and spin can be clicked again.
     $('#spin').removeClass("disabled");
 }
 
